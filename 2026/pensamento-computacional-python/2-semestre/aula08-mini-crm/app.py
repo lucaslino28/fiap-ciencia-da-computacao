b3ca8782677c49135e03960ab8f151da091dcd5f
lucaslino28 <lucaslino2804@gmail.com>
2026-09-17 22:40:02 -0300

    Realizamos em aula a segunda parte desse mini projeto crm

--- 2026/pensamento-computacional-python/2-semestre/aula08-mini-crm/app.py
+++ 2026/pensamento-computacional-python/2-semestre/aula08-mini-crm/app.py
@@ -1,40 +1,73 @@
+from re import search
+
 from model import model_lead
 import control
 
 def add_lead():
     name = input("Nome: ")
-    email = input("Email: ")
-    status = input("Status do fluxo de vendas:")
+    email = input("E-mail: ")
+    status = input("Status do fluxo de vendas: ")
 
     # validar os dados
     # agora, preciso modelar os dados
     # para isso, vamos usar o módulo model.py
     # preciso modelar os dados como um dict
-    print(model_lead(name,email,status))
+    print(model_lead(name, email, status))
 
     # com os dados modelados... preciso enviar para o .json
     # vou usar o control para enviar o dicionario do lead
-    control.create_lead(model_lead(name,email,status))
+    control.create_lead(model_lead(name, email, status))
 
-    print("Lead adicionado")
+    print("Lead adicionado (func)")
 
 def list_leads():
     leads = control.read_leads()
-    print(leads)
+    print(f"## | {"Nome" : <10} | E-mail")
+    for i, lead in enumerate(leads):
+        print(f"{i:02d} | {lead["name"]:<10} | {lead["email"]}")
+
+def search_leads():
+    query = input("Buscar por: ").strip()
+    if not query:
+        print("Consulta vazia")
+        return
+
+    # com a query digitada (busca)... preciso enviar para o control
+    # o control irá comparar a query com os dados dp leads.json
+    # e irá retornar os resultados da busca
+    found_leads = control.read_leads_search(query)
+
+    print(f"## | {"Nome" : <10} | E-mail")
+    for i, lead in found_leads:
+        print(f"{i:02d} | {lead["name"]:<10} | {lead["email"]}")
+
+def export_leads():
+    path_csv = control.export_leads_csv()
+
+    if path_csv:
+        print(f"Não foi possível exportar")
+    else:
+        print(f"Exportado para {path_csv}")
 
 def main():
     while True:
         print("\nMini CRM de Leads")
-        print("[1] Adicionar Lead")
-        print("[2] Listar Leads")
+        print("[1] Adicionar lead")
+        print("[2] Listar leads")
+        print("[3] Buscar (nome/e-mail)")
+        print("[4] Exportar para CSV")
         print("[0] Sair do programa")
 
-        opt = input("Escolha uma opção")
+        opt = input("Escolha uma opção: ")
 
         if opt == "1":
             add_lead()
         elif opt == "2":
             list_leads()
+        elif opt == "3":
+            search_leads()
+        elif opt == "4":
+            export_leads()
         elif opt == "0":
             print("Até mais...")
             break
--- 2026/pensamento-computacional-python/2-semestre/aula08-mini-crm/control.py
+++ 2026/pensamento-computacional-python/2-semestre/aula08-mini-crm/control.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-import json
+import json, csv
 
 DATA_DIR = Path(__file__).resolve().parent / "data"
 DATA_DIR.mkdir(exist_ok=True)
@@ -25,4 +25,33 @@ def read_leads():
 def create_lead(lead_dict):
     leads = read_leads()
     leads.append(lead_dict)
-    DB_PATH.write_text(json.dumps(leads, ensure_ascii=False, ident=2), encoding="utf-8")
+    DB_PATH.write_text(json.dumps(leads, ensure_ascii=False, indent=2), encoding="utf-8")
+
+# FUNÇÃO QUE RECEBE O TEXTO DA BUSCA E RETORNA UMA LISTA COM RESULTADOS
+def read_leads_search(query):
+    leads = read_leads() # lista de dicionarios / lista de leads / array of dicts
+    results = []
+
+    for i, lead in enumerate(leads):
+        txt_lead = f"{lead["name"]}".lower()
+
+        if query.lower() in txt_lead:
+            results.append((i, lead))
+
+    return results
+
+# EXPORTAR LEADS COMO CSV
+def export_csv():
+    """Exporta leads para CSV e RETORNA o caminho do arquivo"""
+    path_csv = DATA_DIR / "leads.csv"
+
+    leads = read_leads()
+    try:
+        with path_csv.open("w", newLine="", encoding="utf-8") as file_csv:
+            writer = csv.DictWriter(file_csv, leads[0].kiys())
+            writer.writeheader()
+            for row_dict in leads:
+                writer.writerow(row_dict)
+        return path_csv
+    except PermissionError:
+        return None
